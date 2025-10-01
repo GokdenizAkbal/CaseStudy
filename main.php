c7045bb700a528fff297da88ddbda2317d07a7a0
GokdenizAkbal <akbalgokdeniz@gmail.com>
2025-10-02 01:23:28 +0300

    products.json is added and productCard.php is modified according to json data.

--- main.php
+++ main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Case Study</title>
     <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
 
--- productCard.php
+++ productCard.php
@@ -1,91 +1,49 @@
+<?php
+$jsonData = file_get_contents('products.json');
+$products = json_decode($jsonData, true);
+?>
+
 <h1>Product List</h1>
 <div class="product-list">
-    <div class="product-card">
-        <img src="Images/ring.png" alt="Product Image">
-        <h2>Product Title</h2>
-        <p class="price">$101.00 USD</p>
+    <?php foreach($products as $product): ?>
+        <div class="product-card">
+            <img src="<?php echo $product['images']['yellow']; ?>" alt="<?php echo htmlspecialchars($product['name']); ?>" class="product-img">
+            <h2><?php echo htmlspecialchars($product['name']); ?></h2>
+            <p class="price">$<?php echo number_format($product['weight'] * 50, 2); ?> USD</p>
 
-        <div class="colors">
-            <span class="color gold"></span>
-            <span class="color silver"></span>
-            <span class="color pink"></span>
-        </div>
-        <p class="color-name">Yellow Gold</p>
-
-        <div class="rating" data-rating="4.5">
-        <div class="stars-outer">
-            <div class="stars-inner"></div>
-        </div>
-            <span>4.5/5</span>
-        </div>
-    </div>
-
-    <div class="product-card">
-        <img src="Images/ring.png" alt="Product Image">
-        <h2>Product Title</h2>
-        <p class="price">$101.00 USD</p>
-
-        <div class="colors">
-            <span class="color gold"></span>
-            <span class="color silver"></span>
-            <span class="color pink"></span>
-        </div>
-        <p class="color-name">Yellow Gold</p>
-
-        <div class="rating" data-rating="2.5">
-            <div class="stars-outer">
-                <div class="stars-inner"></div>
+            <div class="colors">
+                <span class="color yellow" data-img="<?php echo $product['images']['yellow']; ?>"></span>
+                <span class="color rose" data-img="<?php echo $product['images']['rose']; ?>"></span>
+                <span class="color white" data-img="<?php echo $product['images']['white']; ?>"></span>
             </div>
-            <span>2.5/5</span>
-        </div>
-    </div>
+            <p class="color-name">Yellow Gold</p>
 
-    <div class="product-card">
-        <img src="Images/ring.png" alt="Product Image">
-        <h2>Product Title</h2>
-        <p class="price">$101.00 USD</p>
-
-        <div class="colors">
-            <span class="color gold"></span>
-            <span class="color silver"></span>
-            <span class="color pink"></span>
-        </div>
-        <p class="color-name">Yellow Gold</p>
-
-        <div class="rating" data-rating="4">
-            <div class="stars-outer">
-                <div class="stars-inner"></div>
+            <div class="rating" data-rating="<?php echo $product['popularityScore'] * 5; ?>">
+                <div class="stars-outer">
+                    <div class="stars-inner"></div>
+                </div>
+                <span><?php echo round($product['popularityScore'] * 5, 1); ?>/5</span>
             </div>
-            <span>4.0/5</span>
         </div>
-    </div>
-
-    <div class="product-card">
-        <img src="Images/ring.png" alt="Product Image">
-        <h2>Product Title</h2>
-        <p class="price">$101.00 USD</p>
-
-        <div class="colors">
-            <span class="color gold"></span>
-            <span class="color silver"></span>
-            <span class="color pink"></span>
-        </div>
-        <p class="color-name">Yellow Gold</p>
-
-        <div class="rating" data-rating="4.5">
-            <div class="stars-outer">
-                <div class="stars-inner"></div>
-            </div>
-            <span>4.5/5</span>
-        </div>
-    </div>
+    <?php endforeach; ?>
 </div>
 
-
 <script>
+
     document.querySelectorAll('.rating').forEach(rating => {
         const value = parseFloat(rating.getAttribute('data-rating'));
         const percent = (value / 5) * 100;
         rating.querySelector('.stars-inner').style.width = percent + '%';
+    });
+
+
+    document.querySelectorAll('.product-card').forEach(card => {
+        const mainImg = card.querySelector('.product-img');
+        const colorSpans = card.querySelectorAll('.colors .color');
+        colorSpans.forEach(span => {
+            span.addEventListener('click', () => {
+                mainImg.src = span.getAttribute('data-img');
+            });
+        });
     });
 </script>
